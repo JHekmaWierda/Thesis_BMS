a54f418824cca4ccbb45efd4435bc6c3c34088dc
JHekmaWierda <jolijn.hekmawierda@gmail.com>
2023-05-30 23:14:10 +0200

    Update Creating a new style cytoscape.R

--- Code/Creating a new style cytoscape.R
+++ Code/Creating a new style cytoscape.R
@@ -5,10 +5,11 @@ defaults <- list(NODE_SHAPE="ellipse",
                  NODE_LABEL_POSITION="c,c,c,0.00,0.00")
 nodeLabels <- mapVisualProperty('node label','display name','p')
 nodeFills <- mapVisualProperty('node fill color','logFC_neuro',"c",c(-0.28,0,0.28), c("#1458FF","#FFFFFF","#FE3B0B"))
-nodeborder <- mapVisualProperty('node border paint', 'P.value_Neuro', 'c', c(0.05,0.050000001),c("#00FF0C","#FFFFFF"))
+nodeborder <- mapVisualProperty('node border paint', 'P.Value_Neuro', 'c', c(0.05,0.050000001),c("#00FF0C","#FFFFFF"))
 arrowShapes <- mapVisualProperty('Edge Target Arrow Shape','interaction','d',
                                  c("activates","inhibits","interacts"),c("Arrow","T","None"))
+pathwayscolour <- mapVisualProperty('node fill color','CTL.Type','pathway',"#DD99FF")
 #and then create the style
-createVisualStyle(style.name.neuro, defaults, list(nodeLabels,nodeFills,arrowShapes,nodeborder))
+createVisualStyle(style.name.neuro, defaults, list(nodeLabels,nodeFills,arrowShapes,nodeborder,pathwayscolour))
 #finsh by applying the style
 setVisualStyle(style.name.neuro)
